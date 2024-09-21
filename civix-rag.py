b2b4cf0f168957299c412f28951e2233cfbe54e7
Juan Zamudio <jzamudio14@gmail.com>
2024-09-21 00:18:44 -0400

    updated the open ai script

--- civix-rag.py
+++ civix-rag.py
@@ -29,15 +29,23 @@ query_engine = index.as_query_engine(similarity_top_k = 3)
 def generate_reponse(query):
 
     retrieved_info = query_engine.query(query)
-    prompt = f"Using the following information: {retrieved_info}, please answer the question in as much detail as possible: {query}"
+    prompt = [
+        {
+            "role": "system", 
+            "content": f"You are a helpful assistant that promotes civic engagement by guiding users and answering their questions regarding different events and resources that exist in their city. Use the following information to base your answers: {retrieved_info}. Please answer the user's question in as much detail as possible"
+        },
+        {
+            "role": "user", 
+            "content": f"{query}"}
+    ]
 
-    response = openai.completions.create(
-        model="gpt-3.5-turbo-instruct",
-        prompt = prompt,
+    response = openai.chat.completions.create(
+        model="gpt-4o-mini",
+        messages = prompt,
         max_tokens = 150
     )
 
-    return response.choices[0].text.strip()
+    return response.choices[0].message.content.strip()
 
 query = input("What's your question?")
 print(generate_reponse(query))
